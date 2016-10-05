819abcbb23b9779af26d4a3b8f121af1e56ac1ff
CamiloCeronGomez <camiloceron1990@gmail.com>
2016-10-05 10:42:20 -0500

    Valores en grados desde o a 360 para los 3 ejes

--- app/src/main/java/com/example/camilo/vectorderotacion/MainActivity.java
+++ app/src/main/java/com/example/camilo/vectorderotacion/MainActivity.java
@@ -5,19 +5,35 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private TextView textViewX;
     private TextView textViewY;
     private TextView textViewZ;
     private TextView textViewE;
-
-    private SensorManager sensorManager;
+    private int Azimuth = 0;
+    private int Pitch = 0;
+    private int Roll = 0;
+    float[] orientation = new float[3];
+    float[] rMat = new float[9];
+    private SensorManager sensorManager = null;
     private Sensor vectorRotacion;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,37 +49,91 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         // definicion del tipó de sensor que sera utilizado
         vectorRotacion = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-// Metodo para iniciar la captura del vector de rotacion
-    protected void onResume(){
+    // Metodo para iniciar la captura del vector de rotacion
+    protected void onResume() {
         super.onResume();
         // El parametro SENSOR_DELAY_NORMAL define la velocidad de captura del sensor
-        sensorManager.registerListener(this, vectorRotacion, sensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener((SensorEventListener) this, vectorRotacion, sensorManager.SENSOR_DELAY_NORMAL);
     }
 
     // Metodo para parar cuando no se esta interactuando con el susario, esto reduce el consumo de energia
     protected void onPause() {
         super.onPause();
-        sensorManager.unregisterListener(this);
+        sensorManager.unregisterListener((SensorEventListener) this);
     }
+
+
     // Accionado cuando los valores del sensor cambian
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        Float x = sensorEvent.values[0];
-        Float y = sensorEvent.values[1];
-        Float z = sensorEvent.values[2];
-        Float e = sensorEvent.values[3];
+    public void onSensorChanged(SensorEvent event) {
 
+        switch (event.sensor.getType()) {
+            // Valores de la aceleracion del dispositivo incluyendo la gravedad
+            case Sensor.TYPE_ROTATION_VECTOR:
 
-        textViewX.setText("Valor en X: " + x.toString());
-        textViewY.setText("Valor en Y: " + y.toString());
-        textViewZ.setText("Valor en Z: " + z.toString());
-        textViewE.setText("Magnitud Escalar: " + e.toString());
+                //Calcular la Matris de Rotacion
+                SensorManager.getRotationMatrixFromVector(rMat, event.values);
+                // Obtener el valor de Azimuth en Grados
+                Azimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+                Pitch = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[1]) + 360) % 360;
+                Roll = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[2]) + 360) % 360;
+
+                textViewX.setText("Posicion X: " + Azimuth);
+                textViewY.setText("Posicion Y: " + Pitch);
+                textViewZ.setText("Posicion Z: " + Roll);
+
+                break;
+
+        }
     }
-    // Cuando hay un cambio en la presicion del sensor
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.camilo.vectorderotacion/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.camilo.vectorderotacion/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
